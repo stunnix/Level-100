2167847aeb9ec2daf1974567b75a40c9f858f77a
Kingston <tinybirdieyt@gmail.com>
2026-05-15 15:44:25 -1000

    PlayerCasting

---
+++ PlayerCasting.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour
+{
+    public static float distanceFromTarget;
+    [SerializeField] float toTarget;
+
+    void Update()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
+            toTarget = Hit.distance;
+            distanceFromTarget = toTarget;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
